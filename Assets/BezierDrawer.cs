cc6d4cbf3a8c8d7c8bc195bc2026abcc4ed7a5ef
Adrien Leroy <leroy.adrien.76@gmail.com>
2020-10-02 14:56:38 +0200

    Dessiner facilement des courbes de Bézier quadratiques

---
+++ Assets/BezierDrawer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BezierDrawer : MonoBehaviour
+{
+    public int nbPoints = 50;
+    private Vector3[] positions = new Vector3[50];
+    public LineRenderer lineRenderer;
+    public Transform position0;
+    public Transform position1;
+    public Transform position2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = nbPoints;
+        DrawLineCurve(position0.position, position1.position, position2.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawLineCurve(Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        for (int i = 1; i < nbPoints + 1; i++)
+        {
+            float t = i / (float) nbPoints;
+            print(t);
+            positions[i - 1] = BezierCurve(t, p0, p1, p2);
+            
+        }
+
+        lineRenderer.SetPositions(positions);
+    }
+    Vector3 BezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        return (1 - t) * (1 - t) * p0 + 2 * p1 * t * (1 - t) + p2 * t * t;
+    }
+}
---
+++ Assets/FollowPath.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public Transform position0;
+    public Transform position1;
+    public Transform position2;
+    int speed = 1;
+    int nbPoints=100;
+    public Vector3[] positions = new Vector3[1];
+    Coroutine MoveIE;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new Vector3[nbPoints];
+        transform.position=position0.position;
+        DrawLineCurve(position0.position, position1.position, position2.position);
+        StartCoroutine(moveObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator moveObject()
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            MoveIE = StartCoroutine(Moving(i));
+            yield return MoveIE;
+        }
+    }
+
+    IEnumerator Moving(int currentPosition)
+    {
+        while (transform.position != positions[currentPosition])
+        {
+            transform.position = Vector3.MoveTowards(transform.position, positions[currentPosition], speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    Vector3 BezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        return (1 - t) * (1 - t) * p0 + 2 * p1 * t * (1 - t) + p2 * t * t;
+    }
+
+        void DrawLineCurve(Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        for (int i = 1; i < nbPoints + 1; i++)
+        {
+            float t = i / (float) nbPoints;
+            print(t);
+            positions[i - 1] = BezierCurve(t, p0, p1, p2);
+            
+        }
+    }
+}   
